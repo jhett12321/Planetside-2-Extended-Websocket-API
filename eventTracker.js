84b1e6ba5507c44ecff4c211a380a4a28e167233
Jhett Black <jhettblack@hotmail.com>
2014-08-14 21:30:51 +1000

    Added Open Source license info.

--- eventTracker.js
+++ eventTracker.js
@@ -1,4 +1,22 @@
-//This file processes the raw data provided by SOE's Census REST API, and saves the data to a local database, and a relay Websocket Server.
+/*
+    This file is a part of the Planetside 2 Event Tracker program.
+	The Event Tracker receives, filters and relays all event data sent by the SOE Census API. <http://census.soe.com/>
+	
+    Copyright (C) 2014  Jhett Black
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 //Modules
 var WebSocket = require('ws');
