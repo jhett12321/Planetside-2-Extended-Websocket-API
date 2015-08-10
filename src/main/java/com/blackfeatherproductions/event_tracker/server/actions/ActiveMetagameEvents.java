f7c59c88296c1e11194185c8988c4b16ab7d0155
Jhett Black <jhettblack@hotmail.com>
2015-08-10 23:56:34 +1000

    Don't return empty worlds, added some fields to the "activeMetagameEvent" actions.

--- src/main/java/com/blackfeatherproductions/event_tracker/server/actions/ActiveMetagameEvents.java
+++ src/main/java/com/blackfeatherproductions/event_tracker/server/actions/ActiveMetagameEvents.java
@@ -14,6 +14,7 @@ import com.blackfeatherproductions.event_tracker.data_dynamic.WorldInfo;
 import com.blackfeatherproductions.event_tracker.data_static.Facility;
 import com.blackfeatherproductions.event_tracker.data_static.World;
 import com.blackfeatherproductions.event_tracker.utils.CensusUtils;
+import com.blackfeatherproductions.event_tracker.utils.TerritoryUtils;
 
 @ActionInfo(actionNames = "activeMetagameEvents")
 public class ActiveMetagameEvents implements Action
@@ -50,6 +51,15 @@ public class ActiveMetagameEvents implements Action
                             metagameEvent.put("end_time", metagameEventInfo.getEndTime());
                             metagameEvent.put("facility_type_id", metagameEventInfo.getType().getFacilityType().getID());
                             metagameEvent.put("category_id", metagameEventInfo.getType().getCategoryID());
+                            
+                            JsonObject controlInfo = TerritoryUtils.calculateTerritoryControl(world, metagameEventInfo.getType().getZone());
+                            String control_vs = controlInfo.getString("control_vs");
+                            String control_nc = controlInfo.getString("control_nc");
+                            String control_tr = controlInfo.getString("control_tr");
+                            
+                            metagameEvent.put("control_vs", control_vs);
+                            metagameEvent.put("control_nc", control_nc);
+                            metagameEvent.put("control_tr", control_tr);
 
                             JsonArray facilities = new JsonArray();
 
@@ -75,43 +85,56 @@ public class ActiveMetagameEvents implements Action
                 }
             }
         }
+        
         else
         {
             for (Entry<World, WorldInfo> world : dynamicDataManager.getAllWorldInfo().entrySet())
             {
-                JsonArray metagameEvents = new JsonArray();
-
-                for (MetagameEventInfo metagameEventInfo : world.getValue().getActiveMetagameEvents().values())
+                if(world.getValue().getActiveMetagameEvents().size() > 0)
                 {
-                    JsonObject metagameEvent = new JsonObject();
+                    JsonArray metagameEvents = new JsonArray();
 
-                    metagameEvent.put("instance_id", metagameEventInfo.getInstanceID());
-                    metagameEvent.put("metagame_event_type_id", metagameEventInfo.getType().getID());
-                    metagameEvent.put("start_time", metagameEventInfo.getStartTime());
-                    metagameEvent.put("end_time", metagameEventInfo.getEndTime());
-                    metagameEvent.put("facility_type_id", metagameEventInfo.getType().getFacilityType().getID());
-                    metagameEvent.put("category_id", metagameEventInfo.getType().getCategoryID());
-
-                    JsonArray facilities = new JsonArray();
-
-                    for (Entry<Facility, FacilityInfo> facilityInfo : world.getValue().getZoneInfo(metagameEventInfo.getType().getZone()).getFacilities().entrySet())
+                    for (MetagameEventInfo metagameEventInfo : world.getValue().getActiveMetagameEvents().values())
                     {
-                        JsonObject facility = new JsonObject();
+                        JsonObject metagameEvent = new JsonObject();
 
-                        facility.put("facility_id", facilityInfo.getKey().getID());
-                        facility.put("facility_type_id", facilityInfo.getKey().getType().getID());
-                        facility.put("owner", facilityInfo.getValue().getOwner().getID());
-                        facility.put("zone_id", metagameEventInfo.getType().getZone().getID());
+                        metagameEvent.put("instance_id", metagameEventInfo.getInstanceID());
+                        metagameEvent.put("metagame_event_type_id", metagameEventInfo.getType().getID());
+                        metagameEvent.put("start_time", metagameEventInfo.getStartTime());
+                        metagameEvent.put("end_time", metagameEventInfo.getEndTime());
+                        metagameEvent.put("facility_type_id", metagameEventInfo.getType().getFacilityType().getID());
+                        metagameEvent.put("category_id", metagameEventInfo.getType().getCategoryID());
+                        
+                        JsonObject controlInfo = TerritoryUtils.calculateTerritoryControl(world.getKey(), metagameEventInfo.getType().getZone());
+                        String control_vs = controlInfo.getString("control_vs");
+                        String control_nc = controlInfo.getString("control_nc");
+                        String control_tr = controlInfo.getString("control_tr");
 
-                        facilities.add(facility);
+                        metagameEvent.put("control_vs", control_vs);
+                        metagameEvent.put("control_nc", control_nc);
+                        metagameEvent.put("control_tr", control_tr);
+
+                        JsonArray facilities = new JsonArray();
+
+                        for (Entry<Facility, FacilityInfo> facilityInfo : world.getValue().getZoneInfo(metagameEventInfo.getType().getZone()).getFacilities().entrySet())
+                        {
+                            JsonObject facility = new JsonObject();
+
+                            facility.put("facility_id", facilityInfo.getKey().getID());
+                            facility.put("facility_type_id", facilityInfo.getKey().getType().getID());
+                            facility.put("owner", facilityInfo.getValue().getOwner().getID());
+                            facility.put("zone_id", metagameEventInfo.getType().getZone().getID());
+
+                            facilities.add(facility);
+                        }
+
+                        metagameEvent.put("facilities", facilities);
+
+                        metagameEvents.add(metagameEvent);
                     }
 
-                    metagameEvent.put("facilities", facilities);
-
-                    metagameEvents.add(metagameEvent);
+                    worlds.put(world.getKey().getID(), metagameEvents);
                 }
-
-                worlds.put(world.getKey().getID(), metagameEvents);
             }
         }
 
